e85466ee3b63f49a9ac4e22aab5bda5ac82fca42
Monica Weitekamp <monicadweitekamp@gmail.com>
2025-08-19 16:40:26 -0400

    Format code

--- src/App.js
+++ src/App.js
@@ -9,20 +9,20 @@ const controlJsonLink = 'https://s3.us-east-2.amazonaws.com/bww-1/USNicheGradeSa
 const bwwLogo = 'https://logos-world.net/wp-content/uploads/2022/01/Buffalo-Wild-Wings-Symbol.png';
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            bwwAreas: [],
-            controlAreas: []
-        }
+	constructor(props) {
+		super(props)
+		this.state = {
+			bwwAreas: [],
+			controlAreas: []
+		}
 
-		axios.get(bwwJsonLink).then(res => { this.setState({ bwwAreas: res.data })});
-		axios.get(controlJsonLink).then(res => { this.setState({ controlAreas: res.data })});
-    }
+		axios.get(bwwJsonLink).then(res => { this.setState({ bwwAreas: res.data }) });
+		axios.get(controlJsonLink).then(res => { this.setState({ controlAreas: res.data }) });
+	}
 
-	 numberWithCommas = (x) => {
-	    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-	 }
+	numberWithCommas = (x) => {
+		return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+	}
 
 	render() {
 		return (
@@ -32,34 +32,34 @@ class App extends Component {
 					<h1 className="App-title">The Buffalo Wild Wings Project</h1>
 				</header>
 
-			<p className="App-intro">Project to test the hypothesis that neighborhoods are better where BWWs are built</p>
+				<p className="App-intro">Project to test the hypothesis that neighborhoods are better where BWWs are built</p>
 
-			<div className="container">
-				{this.state.bwwAreas.length && <div className="row">
-					<div className="col">
-						<h2><strong>{this.numberWithCommas(this.state.bwwAreas.length-3)}</strong></h2>
-						<h4>BWW Locations</h4>
+				<div className="container">
+					{this.state.bwwAreas.length && <div className="row">
+						<div className="col">
+							<h2><strong>{this.numberWithCommas(this.state.bwwAreas.length - 3)}</strong></h2>
+							<h4>BWW Locations</h4>
 						</div>
-					<div className="col">
-						<h2><strong>{this.numberWithCommas((this.state.bwwAreas.length-3)*2*12)}</strong></h2>
-						<h4>Area Ratings</h4>
-					</div>
-					<div className="col">
-						<h2><strong>Never too many</strong></h2>
-						<h4>Buffalo Wings</h4>
-					</div>
-				</div> }
-			</div>
+						<div className="col">
+							<h2><strong>{this.numberWithCommas((this.state.bwwAreas.length - 3) * 2 * 12)}</strong></h2>
+							<h4>Area Ratings</h4>
+						</div>
+						<div className="col">
+							<h2><strong>Never too many</strong></h2>
+							<h4>Buffalo Wings</h4>
+						</div>
+					</div>}
+				</div>
 
-			<br/><br/>
+				<br /><br />
 
-			<OverallChart />
-			<br/><br/>
+				<OverallChart />
+				<br /><br />
 
 
-			<div className="container">
-				<TableauViz />
-			</div>
+				<div className="container">
+					<TableauViz />
+				</div>
 			</div>
 		);
 	}
--- src/OverallChart.js
+++ src/OverallChart.js
@@ -9,95 +9,97 @@ const bwwJsonLink = 'https://s3.us-east-2.amazonaws.com/bww-1/BuffaloWildWings2.
 const controlJsonLink = 'https://s3.us-east-2.amazonaws.com/bww-1/USNicheGradeSample.json';
 
 class OverallChart extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            bwwAreas: [], controlAreas: [],
-            bwwAPlus: [], bwwA: [], bwwAMinus: [],
-            bwwBPlus: [], bwwB: [], bwwBMinus: [],
-            bwwCPlus: [], bwwC: [], bwwCMinus: [],
-            bwwDPlus: [], bwwD: [], bwwDMinus: [],
+	constructor(props) {
+		super(props)
+		this.state = {
+			bwwAreas: [], controlAreas: [],
+			bwwAPlus: [], bwwA: [], bwwAMinus: [],
+			bwwBPlus: [], bwwB: [], bwwBMinus: [],
+			bwwCPlus: [], bwwC: [], bwwCMinus: [],
+			bwwDPlus: [], bwwD: [], bwwDMinus: [],
 
-            controlAPlus: [], controlA: [], controlAMinus: [],
-            controlBPlus: [], controlB: [], controlBMinus: [],
-            controlCPlus: [], controlC: [], controlCMinus: [],
-            controlDPlus: [], controlD: [], controlDMinus: []
-        }
+			controlAPlus: [], controlA: [], controlAMinus: [],
+			controlBPlus: [], controlB: [], controlBMinus: [],
+			controlCPlus: [], controlC: [], controlCMinus: [],
+			controlDPlus: [], controlD: [], controlDMinus: []
+		}
 
 		axios.get(bwwJsonLink)
-		.then(res => {
-			const bwwAreas = res.data;
-			const bwwAPlus = bwwAreas.filter(item=>item['Overall Niche Grade']==="A+");
-			const bwwA = bwwAreas.filter(item=>item['Overall Niche Grade']==="A");
-			const bwwAMinus = bwwAreas.filter(item=>item['Overall Niche Grade']==="A-");
-			const bwwBPlus = bwwAreas.filter(item=>item['Overall Niche Grade']==="B+");
-			const bwwB = bwwAreas.filter(item=>item['Overall Niche Grade']==="B");
-			const bwwBMinus = bwwAreas.filter(item=>item['Overall Niche Grade']==="B-");
-			const bwwCPlus = bwwAreas.filter(item=>item['Overall Niche Grade']==="C+");
-			const bwwC = bwwAreas.filter(item=>item['Overall Niche Grade']==="C");
-			const bwwCMinus = bwwAreas.filter(item=>item['Overall Niche Grade']==="C-");
-			const bwwDPlus = bwwAreas.filter(item=>item['Overall Niche Grade']==="D+");
-			const bwwD = bwwAreas.filter(item=>item['Overall Niche Grade']==="D");
-			const bwwDMinus = bwwAreas.filter(item=>item['Overall Niche Grade']==="D-");
+			.then(res => {
+				const bwwAreas = res.data;
+				const bwwAPlus = bwwAreas.filter(item => item['Overall Niche Grade'] === "A+");
+				const bwwA = bwwAreas.filter(item => item['Overall Niche Grade'] === "A");
+				const bwwAMinus = bwwAreas.filter(item => item['Overall Niche Grade'] === "A-");
+				const bwwBPlus = bwwAreas.filter(item => item['Overall Niche Grade'] === "B+");
+				const bwwB = bwwAreas.filter(item => item['Overall Niche Grade'] === "B");
+				const bwwBMinus = bwwAreas.filter(item => item['Overall Niche Grade'] === "B-");
+				const bwwCPlus = bwwAreas.filter(item => item['Overall Niche Grade'] === "C+");
+				const bwwC = bwwAreas.filter(item => item['Overall Niche Grade'] === "C");
+				const bwwCMinus = bwwAreas.filter(item => item['Overall Niche Grade'] === "C-");
+				const bwwDPlus = bwwAreas.filter(item => item['Overall Niche Grade'] === "D+");
+				const bwwD = bwwAreas.filter(item => item['Overall Niche Grade'] === "D");
+				const bwwDMinus = bwwAreas.filter(item => item['Overall Niche Grade'] === "D-");
 
-			this.setState({ bwwAreas: bwwAreas,
-				bwwAPlus: bwwAPlus, bwwA: bwwA, bwwAMinus: bwwAMinus,
-				bwwBPlus: bwwBPlus, bwwB: bwwB, bwwBMinus: bwwBMinus,
-				bwwCPlus: bwwCPlus, bwwC: bwwC, bwwCMinus: bwwCMinus,
-				bwwDPlus: bwwDPlus, bwwD: bwwD, bwwDMinus: bwwDMinus
+				this.setState({
+					bwwAreas: bwwAreas,
+					bwwAPlus: bwwAPlus, bwwA: bwwA, bwwAMinus: bwwAMinus,
+					bwwBPlus: bwwBPlus, bwwB: bwwB, bwwBMinus: bwwBMinus,
+					bwwCPlus: bwwCPlus, bwwC: bwwC, bwwCMinus: bwwCMinus,
+					bwwDPlus: bwwDPlus, bwwD: bwwD, bwwDMinus: bwwDMinus
+				});
+
 			});
+	}
 
-		});
-    }
-	
 	componentDidMount() {
 		axios.get(controlJsonLink)
-		.then(res => {
-			const controlAreas = res.data;
-			const controlAPlus = controlAreas.filter(item=>item['Overall Niche Grade']==="A+");
-			const controlA = controlAreas.filter(item=>item['Overall Niche Grade']==="A");
-			const controlAMinus = controlAreas.filter(item=>item['Overall Niche Grade']==="A-");
-			const controlBPlus = controlAreas.filter(item=>item['Overall Niche Grade']==="B+");
-			const controlB = controlAreas.filter(item=>item['Overall Niche Grade']==="B");
-			const controlBMinus = controlAreas.filter(item=>item['Overall Niche Grade']==="B-");
-			const controlCPlus = controlAreas.filter(item=>item['Overall Niche Grade']==="C+");
-			const controlC = controlAreas.filter(item=>item['Overall Niche Grade']==="C");
-			const controlCMinus = controlAreas.filter(item=>item['Overall Niche Grade']==="C-");
-			const controlDPlus = controlAreas.filter(item=>item['Overall Niche Grade']==="D+");
-			const controlD = controlAreas.filter(item=>item['Overall Niche Grade']==="D");
-			const controlDMinus = controlAreas.filter(item=>item['Overall Niche Grade']==="D-");
+			.then(res => {
+				const controlAreas = res.data;
+				const controlAPlus = controlAreas.filter(item => item['Overall Niche Grade'] === "A+");
+				const controlA = controlAreas.filter(item => item['Overall Niche Grade'] === "A");
+				const controlAMinus = controlAreas.filter(item => item['Overall Niche Grade'] === "A-");
+				const controlBPlus = controlAreas.filter(item => item['Overall Niche Grade'] === "B+");
+				const controlB = controlAreas.filter(item => item['Overall Niche Grade'] === "B");
+				const controlBMinus = controlAreas.filter(item => item['Overall Niche Grade'] === "B-");
+				const controlCPlus = controlAreas.filter(item => item['Overall Niche Grade'] === "C+");
+				const controlC = controlAreas.filter(item => item['Overall Niche Grade'] === "C");
+				const controlCMinus = controlAreas.filter(item => item['Overall Niche Grade'] === "C-");
+				const controlDPlus = controlAreas.filter(item => item['Overall Niche Grade'] === "D+");
+				const controlD = controlAreas.filter(item => item['Overall Niche Grade'] === "D");
+				const controlDMinus = controlAreas.filter(item => item['Overall Niche Grade'] === "D-");
 
-			this.setState({ controlAreas: controlAreas,
-			    controlAPlus: controlAPlus, controlA: controlA, controlAMinus: controlAMinus,
-				controlBPlus: controlBPlus, controlB: controlB, controlBMinus: controlBMinus,
-				controlCPlus: controlCPlus, controlC: controlC, controlCMinus: controlCMinus,
-				controlDPlus: controlDPlus, controlD: controlD, controlDMinus: controlDMinus
-			});
-		})
+				this.setState({
+					controlAreas: controlAreas,
+					controlAPlus: controlAPlus, controlA: controlA, controlAMinus: controlAMinus,
+					controlBPlus: controlBPlus, controlB: controlB, controlBMinus: controlBMinus,
+					controlCPlus: controlCPlus, controlC: controlC, controlCMinus: controlCMinus,
+					controlDPlus: controlDPlus, controlD: controlD, controlDMinus: controlDMinus
+				});
+			})
 	}
 
 	render() {
 		const config = {
 			chart: { type: 'column' },
-			title: {text: 'Overall Grades by Zip Code'},
-			subtitle: {text: 'Source: Niche.com'},
+			title: { text: 'Overall Grades by Zip Code' },
+			subtitle: { text: 'Source: Niche.com' },
 			xAxis: {
 				categories: [
-				'A+','A','A-',
-				'B+','B','B-',
-				'C+','C','C-',
-				'D+','D','D-'
+					'A+', 'A', 'A-',
+					'B+', 'B', 'B-',
+					'C+', 'C', 'C-',
+					'D+', 'D', 'D-'
 				],
 				crosshair: true
 			},
 			yAxis: {
 				min: 0,
-				title: {text: 'Frequency'}
+				title: { text: 'Frequency' }
 			},
 			tooltip: {
 				headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
 				pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-				'<td style="padding:0"><b>{point.y:.0f} </b></td></tr>',
+					'<td style="padding:0"><b>{point.y:.0f} </b></td></tr>',
 				footerFormat: '</table>',
 				shared: true,
 				useHTML: true
